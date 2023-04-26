3fc0d874cd50bf81fe840270c5e2ad9722d59d7a
Impulse Imp <imulses4mini@gmail.com>
2023-04-26 14:31:39 +0300

    libs installed

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { Navbar, Sidebar, Footer } from './components';
+import { Navbar, Sidebar, Footer } from './components';
 import './App.css';
 
 function App() {
