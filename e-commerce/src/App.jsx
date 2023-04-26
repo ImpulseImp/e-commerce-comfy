af862a1c13b8f85fc31fff936455e74578d35a8c
Impulse Imp <imulses4mini@gmail.com>
2023-04-26 19:52:05 +0300

    a

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,13 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
 import './App.css';
+import styled from 'styled-components';
 
-function App() {
+const Button = styled.button`
+  background: red;
+  color: white;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: navy;
+  }
+`;
+
+const App = () => {
   return (
-    <section>
+    <div>
       <h1>comfy sloth starter</h1>
-    </section>
+      <Button>click me</Button>
+    </div>
   );
-}
+};
 
 export default App;
