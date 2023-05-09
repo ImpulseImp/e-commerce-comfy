b34002a25de3bece457c1a1e2942c6773aad43ac
Impulse <impulses4mini@gmail.com>
2023-05-09 22:21:38 +0300

    delete styled button

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -4,20 +4,6 @@ import './App.css';
 import styled from 'styled-components';
 import { Home } from './pages';
 
-const Button = styled.button`
-  background: red;
-  color: white;
-  padding: 10px;
-  border: none;
-  border-radius: 5px;
-  font-size: 16px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: navy;
-  }
-`;
-
 const App = () => {
   return (
     <Router>
